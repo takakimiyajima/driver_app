fcaf45d621998b7409d4c1a9717b4641b2b64cd3
宮嶋 <takaki.mz@icloud.com>
2016-03-05 04:00:34 +0000

    Finish user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 DriverApp::Application.routes.draw do
-  get "users/show"
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
   resources :users, only: [:show]
   
   root  'main_pages#home'
