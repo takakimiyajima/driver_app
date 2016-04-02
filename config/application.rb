b93664ed47e20408a59cd5747909a049195e68bd
宮嶋 <takaki.mz@icloud.com>
2016-04-02 07:29:45 +0000

    adjust searchform

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,13 @@ module DriverApp
     
    # config.assets.paths << "#{Rails}/app/assets/fonts"
     # パスは1でペーストしたフォルダ
+    
+    #日本時間に設定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.active_record.time_zone_aware_attributes = false
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
